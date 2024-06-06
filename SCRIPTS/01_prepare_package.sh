72e236ad4847e4225d80d2299d77b5e3daa8e9db
NuoFang6 <991921514@qq.com>
2024-06-06 17:20:26 +0800

    fix

--- SCRIPTS/01_prepare_package.sh
+++ SCRIPTS/01_prepare_package.sh
@@ -10,39 +10,25 @@ cp -rf ../feeds.conf.default ./feeds.conf.default
 # 更新 Feeds
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+# 可删除的
+
+uneedpkg="luci-app-control-timewol luci-app-appfilter mdio-tools mdio-netlink libpfring ovpn-dco jool xr_usb_serial_common open-app-filter openvpn freeswitch luci-app-openvpn-server freetdm freeswitch-mod-bcg729"
+uneedpkg="$uneedpkg luci-app-openclash luci-app-netdata"
+
+./scripts/feeds uninstall $uneedpkg
 
 ### 获取额外的 LuCI 应用、主题和依赖 ###
+# 源码
+git clone -b dev --depth 1 https://github.com/vernesong/OpenClash.git package/new/luci-app-openclash
+git clone --depth 1 https://github.com/sirpdboy/sirpdboy-package.git package/sirpdboy-package
+
 # 非源码 ipk
 # UA3F
 mkdir -p ../PATCH/files/etc/pre_install/
 wget -q -O ../PATCH/files/etc/pre_install/ua3f_armv8.ipk $(curl -s https://api.github.com/repos/SunBK201/UA3F/releases/latest | grep browser_download_url | grep armv8.ipk | cut -d '"' -f 4) &
 
-# 源码
-# OpenClash
-git clone -b dev --depth 1 https://github.com/vernesong/OpenClash.git package/new/luci-app-openclash
-
 # 必要 Patch
 cp -rf ../PATCH/attr/200-basename.patch ./feeds/packages/utils/attr/patches/
-
-# 可删除的
-rm -rf feeds/sirpdboy/luci-app-control-timewol/ &
-rm -rf feeds/luci/luci-app-appfilter/ &
-rm -rf package/feeds/luci/luci-app-appfilter/ &
-rm -rf feeds/packages/mdio-tools/ &
-rm -rf package/feeds/packages/mdio-tools/ &
-rm -rf feeds/packages/kernel/mdio-netlink/ &
-rm -rf feeds/packages/libs/libpfring &
-rm -rf feeds/packages/kernel/ovpn-dco &
-rm -rf feeds/packages/net/jool &
-rm -rf feeds/packages/libs/xr_usb_serial_common &
-rm -rf feeds/packages/net/open-app-filter &
-rm -rf feeds/packages/openvpn/ &
-rm -rf package/feeds/packages/openvpn/ &
-rm -rf feeds/telephony/freeswitch/ &
-rm -rf package/feeds/telephony/freeswitch &
-rm -rf package/feeds/luci/luci-app-openvpn-server &
-rm -rf package/feeds/telephony/freetdm &
-rm -rf package/feeds/telephony/freeswitch-mod-bcg729 &
 
 ### 最后的收尾工作 ###
 # 默认开启 Irqbalance
@@ -55,23 +41,22 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 sed -i 's/ImmortalWrt/LynnOS/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/LynnOS/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
 
-# Nginx
-sed -i "s/large_client_header_buffers 2 1k/large_client_header_buffers 4 32k/g" feeds/packages/net/nginx-util/files/uci.conf.template
-sed -i "s/client_max_body_size 128M/client_max_body_size 2048M/g" feeds/packages/net/nginx-util/files/uci.conf.template
-sed -i '/client_max_body_size/a\\tclient_body_buffer_size 8192M;' feeds/packages/net/nginx-util/files/uci.conf.template
-sed -i '/client_max_body_size/a\\tserver_names_hash_bucket_size 128;' feeds/packages/net/nginx-util/files/uci.conf.template
-sed -i '/ubus_parallel_req/a\        ubus_script_timeout 600;' feeds/packages/net/nginx/files-luci-support/60_nginx-luci-support
-sed -ri "/luci-webui.socket/i\ \t\tuwsgi_send_timeout 600\;\n\t\tuwsgi_connect_timeout 600\;\n\t\tuwsgi_read_timeout 600\;" feeds/packages/net/nginx/files-luci-support/luci.locations
-sed -ri "/luci-cgi_io.socket/i\ \t\tuwsgi_send_timeout 600\;\n\t\tuwsgi_connect_timeout 600\;\n\t\tuwsgi_read_timeout 600\;" feeds/packages/net/nginx/files-luci-support/luci.locations
+# # Nginx
+# sed -i "s/large_client_header_buffers 2 1k/large_client_header_buffers 4 32k/g" feeds/packages/net/nginx-util/files/uci.conf.template
+# sed -i "s/client_max_body_size 128M/client_max_body_size 2048M/g" feeds/packages/net/nginx-util/files/uci.conf.template
+# sed -i '/client_max_body_size/a\\tclient_body_buffer_size 8192M;' feeds/packages/net/nginx-util/files/uci.conf.template
+# sed -i '/client_max_body_size/a\\tserver_names_hash_bucket_size 128;' feeds/packages/net/nginx-util/files/uci.conf.template
+# sed -i '/ubus_parallel_req/a\        ubus_script_timeout 600;' feeds/packages/net/nginx/files-luci-support/60_nginx-luci-support
+# sed -ri "/luci-webui.socket/i\ \t\tuwsgi_send_timeout 600\;\n\t\tuwsgi_connect_timeout 600\;\n\t\tuwsgi_read_timeout 600\;" feeds/packages/net/nginx/files-luci-support/luci.locations
+# sed -ri "/luci-cgi_io.socket/i\ \t\tuwsgi_send_timeout 600\;\n\t\tuwsgi_connect_timeout 600\;\n\t\tuwsgi_read_timeout 600\;" feeds/packages/net/nginx/files-luci-support/luci.locations
 
 # 预配置文件
 cp -rf ../PATCH/files ./
 
 # Clash 内核
-mkdir -p files/etc/openclash/core
+# mkdir -p files/etc/openclash/core
 # TODO
 
-chmod +x files/etc/openclash/core/clash*
 chmod +x files/etc/init.d/youhua
 
 wait

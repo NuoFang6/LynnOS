c4ca60d6c419366841c407c53affa4f676e6dbe6
NuoFang <991921514@qq.com>
2024-08-09 19:27:25 +0800

    fix

--- SCRIPTS/01_prepare_package.sh
+++ SCRIPTS/01_prepare_package.sh
@@ -2,11 +2,6 @@
 
 # 这条注释只是为了触发编译
 
-clear
-
-# 使用 O3 级别的优化
-sed -i 's/-Os/-O3/g' ./include/target.mk
-
 # 自定义feed源
 cp -rf ../feeds.conf.default ./feeds.conf.default
 # 更新 Feeds
@@ -22,17 +17,13 @@ uneedpkg="$uneedpkg luci-app-openclash luci-app-netdata"
 # 源码
 git clone -b dev --depth 1 https://github.com/vernesong/OpenClash.git ./package/new/luci-app-openclash >/dev/null
 git clone --depth 1 https://github.com/sirpdboy/sirpdboy-package.git ./package/sirpdboy-package >/dev/null
+rm -rf ./package/sirpdboy-package/luci-app-control-timewol
+
 # 滚回修复无法编译的
 mkdir -p ./2305packages/
 git clone -b openwrt-23.05 --depth 1 https://github.com/immortalwrt/packages.git ./2305packages >/dev/null
 rm -rf ./feeds/packages/lang/ruby
 cp -rf ./2305packages/lang/ruby ./feeds/packages/lang/ruby
-# 非源码 ipk
-# UA3F
-mkdir -p ../PATCH/files/etc/pre_install/
-wget -q -O ../PATCH/files/etc/pre_install/ua3f_armv8.ipk $(curl -s https://api.github.com/repos/SunBK201/UA3F/releases/latest | grep browser_download_url | grep armv8.ipk | cut -d '"' -f 4) &
-
-rm -rf ./package/sirpdboy-package/luci-app-control-timewol
 
 # 必要 Patch
 cp -rf ../PATCH/attr/200-basename.patch ./feeds/packages/utils/attr/patches/
@@ -45,8 +36,6 @@ patch -p1 <../../../PATCH/fw4/04-luci-add-firewall4-nft-rules-file.patch
 popd # 回到原目录
 
 ### 最后的收尾工作 ###
-# 默认开启 Irqbalance
-# sed -i "s/enabled '0'/enabled '1'/g" feeds/packages/utils/irqbalance/files/irqbalance.config
 echo "net.netfilter.nf_conntrack_helper = 1" >>./package/kernel/linux/files/sysctl-nf-conntrack.conf
 
 # 修改默认ip
@@ -55,21 +44,8 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 sed -i 's/ImmortalWrt/LynnOS/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/LynnOS/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
 
-# # Nginx
-# sed -i "s/large_client_header_buffers 2 1k/large_client_header_buffers 4 32k/g" feeds/packages/net/nginx-util/files/uci.conf.template
-# sed -i "s/client_max_body_size 128M/client_max_body_size 2048M/g" feeds/packages/net/nginx-util/files/uci.conf.template
-# sed -i '/client_max_body_size/a\\tclient_body_buffer_size 8192M;' feeds/packages/net/nginx-util/files/uci.conf.template
-# sed -i '/client_max_body_size/a\\tserver_names_hash_bucket_size 128;' feeds/packages/net/nginx-util/files/uci.conf.template
-# sed -i '/ubus_parallel_req/a\        ubus_script_timeout 600;' feeds/packages/net/nginx/files-luci-support/60_nginx-luci-support
-# sed -ri "/luci-webui.socket/i\ \t\tuwsgi_send_timeout 600\;\n\t\tuwsgi_connect_timeout 600\;\n\t\tuwsgi_read_timeout 600\;" feeds/packages/net/nginx/files-luci-support/luci.locations
-# sed -ri "/luci-cgi_io.socket/i\ \t\tuwsgi_send_timeout 600\;\n\t\tuwsgi_connect_timeout 600\;\n\t\tuwsgi_read_timeout 600\;" feeds/packages/net/nginx/files-luci-support/luci.locations
-
 # 预配置文件
 cp -rf ../PATCH/files ./
-
-# Clash 内核
-# mkdir -p files/etc/openclash/core
-# TODO
 
 chmod +x files/etc/init.d/youhua
 
--- SCRIPTS/02_target_R2S.sh
+++ SCRIPTS/02_target_R2S.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-clear
 
+# 非源码 ipk
+# UA3F
+mkdir -p ../PATCH/files/etc/pre_install/
+wget -q -O ../PATCH/files/etc/pre_install/ua3f_armv8.ipk $(curl -s https://api.github.com/repos/SunBK201/UA3F/releases/latest | grep browser_download_url | grep armv8.ipk | cut -d '"' -f 4) &
+
+# 使用 O3 级别的优化
+sed -i 's/-Os/-O3/g' ./include/target.mk
 # 使用专属优化
 patch -p1 <../PATCH/target/target_r2s.patch
 patch -p1 <../PATCH/squashfs/add_zstd_support.patch
@@ -12,6 +18,13 @@ sed -i 's,rootwait,rootwait mitigations=off rootfs_mount_options.threads=4,g' ta
 sed -i 's,"eth1" "eth0","eth0" "eth1",g' target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 
+# 默认开启 Irqbalance
+# sed -i "s/enabled '0'/enabled '1'/g" feeds/packages/utils/irqbalance/files/irqbalance.config
+
+# Clash 内核
+# mkdir -p files/etc/openclash/core
+
+# TODO
 # 预配置文件
 cp -rf ../SEED/R2S/config.seed .config
 
@@ -26,6 +39,7 @@ CONFIG_RANDOMIZE_BASE=y
 CONFIG_RANDOMIZE_MODULE_REGION_FULL=y
 # CONFIG_MPTCP_IPV6 is not set
 # CONFIG_CRYPTO_DEV_ROCKCHIP is not set
+CONFIG_ARM_ROCKCHIP_CPUFREQ=y
 EOL
 cp -rf ../SEED/R2S/config-6.6 target/linux/rockchip/armv8/config-6.6
 chmod -R 755 ./

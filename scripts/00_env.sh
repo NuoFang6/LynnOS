ecb5cd3854cdbe30f079c3648ebcaa9b3bcfd820
悠栾 <ark@oom-wg.dev>
2025-09-17 08:54:47 +0800

    fix

--- scripts/00_env.sh
+++ scripts/00_env.sh
@@ -37,7 +37,7 @@ echo "安装 apt-fast"
 sudo -E apt-get -qq update
 /bin/bash -c "$(curl -sL https://git.io/vokNn)"
 echo "安装编译依赖"
-CUSTOM_DEPENDENCIES="liblz4-dev libffi-dev"
+CUSTOM_DEPENDENCIES="liblz4-dev libffi-dev libfuse-dev"
 DEPENDENCY="${CUSTOM_DEPENDENCIES} ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential bzip2 ccache cmake cpio curl device-tree-compiler ecj fastjar flex gawk gettext gcc-multilib g++-multilib git gnutls-dev gperf haveged help2man intltool lib32gcc-s1 libc6-dev-i386 libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses-dev libpython3-dev libreadline-dev libssl-dev libtool libyaml-dev libz-dev lrzsz mkisofs msmtp nano ninja-build p7zip p7zip-full patch pkgconf python3 python3-pip python3-ply python3-docutils python3-pyelftools qemu-utils re2c rsync scons squashfs-tools subversion swig texinfo uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev zstd"
 sudo -E apt-fast install -y -qq $DEPENDENCY
 echo "清理 apt 缓存"

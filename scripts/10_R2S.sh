32cd632a92f2361e4554e268c0cee53dbb388931
悠栾 <ark@oom-wg.dev>
2025-09-18 17:37:58 +0800

    fix

--- scripts/10_R2S.sh
+++ scripts/10_R2S.sh
@@ -166,4 +166,16 @@ cp -f ${upstreampkg}/libs/libffi/Makefile ./package/feeds/packages/libffi/Makefi
 patch -p0 < ${lynndir}/patch/uwsgi/Makefile.patch
 patch -p0 < ${lynndir}/patch/ua4f/Makefile.patch
 
+echo "修改 r8152 驱动为最新"
+sed -i \
+  -e 's/^PKG_VERSION:=.*/PKG_VERSION:=main-latest/' \
+  -e '/^PKG_SOURCE:=/c\
+PKG_SOURCE_PROTO:=git\
+PKG_SOURCE_URL:=https://github.com/wget/realtek-r8152-linux.git\
+PKG_SOURCE_VERSION:=master\
+PKG_MIRROR_HASH:=skip' \
+  -e '/^PKG_SOURCE_URL:=@IMMORTALWRT/d' \
+  -e '/^PKG_HASH:=/d' \
+  ./package/kernel/r8152/Makefile
+
 echo "结束"

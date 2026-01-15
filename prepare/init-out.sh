c71880cc636e7624b32f8ace4bac6de646103a3c
NuoFang6 <991921514@qq.com>
2026-01-15 18:28:47 +0800

    fix

--- prepare/init-out.sh
+++ prepare/init-out.sh
@@ -177,13 +177,6 @@ p "d 命令已可用"
 
 d paru --noconfirm -S ack antlr3
 
-p "安装 zstd 到宿主机"
-d "cp -rf /usr/sbin/zstd* /usr/local/bin_host/"
-chmod +x "${bin_host}/zstd*"
-p "安装 less 到宿主机"
-d "cp -rf /usr/sbin/less* /usr/local/bin_host/"
-chmod +x "${bin_host}/less*"
-
 p "复制仓库到容器内 ${lynndir}"
 cp -r $GITHUB_WORKSPACE ${workdir_out}/lynnos
 
